7434b3f2ac8d34bcb502757e97357aac9b9c6cd5
Ziad Ahmed <zaid.ahmed@quickcheck.ng>
2023-04-06 14:42:06 +0200

    fix the view

--- dynamic_template/serializers.py
+++ dynamic_template/serializers.py
@@ -1,12 +1,18 @@
 from rest_framework import serializers
 
-from dynamic_template.models import EXAMPLE_SCHEMA
+from dynamic_template.models import Template
 from dynamic_template.validators import JSONSchemaValidator
 
 
 class RequestSerializer(serializers.Serializer):
     data = serializers.JSONField()
 
-    def __init__(self, instance, data=..., **kwargs):
+    def get_data(self, object):
+        return "gg"
+
+    def __init__(self, instance: Template, data=..., **kwargs):
         super().__init__(instance, data, **kwargs)
-        self.fields["data"].validators.append(JSONSchemaValidator(EXAMPLE_SCHEMA))
+        self.fields["data"].validators.append(JSONSchemaValidator(instance.data_format))
+
+    def to_representation(self, instance):
+        return self.initial_data
--- dynamic_template/tests.py
+++ dynamic_template/tests.py
@@ -19,5 +19,5 @@ class RequestTest(APITestCase):
             }
         }
         response = self.client.post(url, data, format="json")
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, data)
